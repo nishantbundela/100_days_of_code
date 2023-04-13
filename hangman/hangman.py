c547b12f8b1b702726850f4d79dadc4507262600
nishantbundela <nishantbundela@gmail.com>
2023-04-13 19:15:43 -0400

    improvements to hangman and added repo address file

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,9 +1,16 @@
-word = "prometheus"
+import hangman_words
+import hangman_art
+import random
+
+word = random.choice(hangman_words.word_list)
 word_list = [*word]
 blanks = []
 lives = 6
 for i in range(len(word)):
   blanks.append("_ ")
+
+print(hangman_art.logo)
+print("Welcome to Hangman\n")
 while lives > 0:
   guess = ""
   for i in blanks:
@@ -12,6 +19,9 @@ while lives > 0:
   entry = input("Enter a letter you think is in the word:").lower()
   indices = [j for j in range(len(word_list)) if word_list[j] == entry]
   if entry in word_list:
+    print("This letter is in the word\n\n")
+    print('Next guess >>>>>>>>>>>')
+    print(f"lives remaining: {lives}")
     indices = [j for j in range(len(word_list)) if word_list[j] == entry]
     if guess == word:
       print("Congratulations!! You have guessed the word correctly")
@@ -19,7 +29,9 @@ while lives > 0:
     for k in indices:
       blanks[k] = entry
   else:
-    print("This letter is not in the word")
+    print("This letter is not in the word\n\n")
+    print('Next guess >>>>>>>>>>>')
     lives -= 1
+    print(f"lives remaining: {lives}")
 if lives == 0:
   print("Game Over All lives have been exhausted!!")
---
+++ repo_address.py
@@ -0,0 +1,1 @@
+repo_address = 'https://github.com/nishantbundela/100_days_of_code.git'
