02d90f84803fac3d2de31dd02334e923ad0d7769
nishantbundela <nishantbundela@gmail.com>
2023-04-23 22:12:53 -0400

    added turtle draw program

---
+++ turtle/turtle_draw.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+
+tt = Turtle()
+
+# mOVINF THE TURETLE
+
+tt.forward(100)
+tt.rt(90)
+tt.forward(100)
+tt.rt(90)
+tt.forward(100)
+tt.rt(90)
+tt.forward(100)
+
+
+screen = Screen()
+screen.exitonclick()
